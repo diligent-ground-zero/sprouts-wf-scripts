6acdbfc5bc8e9d27ed3be556a0d164d333dfd88e
Niko Dovidija <niko@diligentstudios.com>
2025-01-15 12:57:43 +0100

    up

--- src/scripts/home/home.js
+++ src/scripts/home/home.js
@@ -2,7 +2,7 @@ import '../../styles/home.css';
 import SplitType from 'split-type';
 import { horizontalLoop } from '../global/infiniteScrollHelper';
 import gsap from 'gsap';
-
+import CustomEase from 'gsap/CustomEase';
 export default function () {
   return new HomeScripts();
 }
@@ -12,10 +12,63 @@ class HomeScripts {
     this.initTrackTextAnimation();
     this.initStackingCards();
     this.initInfiniteLogoLoop();
+    this.initHeroAnimation();
   }
 
+  initHeroAnimation() {
+    let customEase =
+    'M0,0,C0,0,0.13,0.34,0.238,0.442,0.305,0.506,0.322,0.514,0.396,0.54,0.478,0.568,0.468,0.56,0.522,0.584,0.572,0.606,0.61,0.719,0.714,0.826,0.798,0.912,1,1,1,1';
+    const loadingDuration = sessionStorage.getItem('visited') !== null ? 3 : 1;
+    const rows = [
+      '.custom_text_wrap_one',
+      '.custom_text_wrap_two',
+      '.custom_text_wrap_three'
+    ].map(selector => ({
+      container: document.querySelector(selector),
+      image: document.querySelector(`${selector} .hero_heading_image`),
+      text: document.querySelectorAll(`${selector} .hero_heading_text`)
+    }));
+
+
+  
+    // Set initial states
+    rows.forEach(row => {
+      gsap.set(row.container, { overflow: 'hidden' });
+      // gsap.set(row.image, { scale: 0.5, opacity:0 });
+      // row.text.forEach(text => {
+      //   gsap.set(text, { opacity: 0,y:50 });
+      // })
+    });
+  
+    // Create the animation timeline
+    const tl = gsap.timeline({
+      defaults: {
+        ease: 'power3.out',
+      }
+    });
+  
+    // rows.forEach((row, index) => {
+    //   const stagger = index === 0 
+    //     ? loadingDuration 
+    //     : `+=${0.5}`;
+    //   tl.to(row.image, {
+    //     scale: 1,
+    //     opacity: 1,
+    //     duration: 0.8,
+    //   }, stagger);
+    // });
+    // First animate the containers and images
+  
+    // Then animate the text elements
+    // rows.forEach((row, index) => {
+    //   tl.to(row.text, {
+    //     opacity: 1,
+    //     duration: 1,
+    //   }, index === 0 ? '-=1' : '-=0.7'); // Overlap text animations for smoother flow
+    // });
+  }
+  
   initTrackTextAnimation() {
-    // Target all heading-3 elements within the track-child
     const textElements = document.querySelectorAll('.track-child .heading-3');
 
     // Split text into spans for each heading
